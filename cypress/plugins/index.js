20f122c8b0993a730b541c1bce28046a782dd7ce
Nick Chursin <unickq@outlook.com>
2022-01-06 14:48:16 +0200

    allure

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,10 +17,13 @@
  */
 
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   on("file:preprocessor", cucumber());
+  allureWriter(on, config);
+  return config;
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
